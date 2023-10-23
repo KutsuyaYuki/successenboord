a1fa8c458dce3cc0079ee54c7c9ef8351b746519
Yuki Kutsuya <github@yuki.pet>
2023-10-23 17:01:46 +0200

    files now get saved

--- modules/gr_interface.py
+++ modules/gr_interface.py
@@ -2,9 +2,12 @@ import gradio as gr
 import numpy as np
 from PIL import Image, ImageDraw, ImageFont
 from .image_processing import overlay_images, add_text_to_image
+import os
+from datetime import datetime
 
-# Initialize A4 dimensions
-A4_WIDTH, A4_HEIGHT = 2480, 3508
+# Initialize new template dimensions and total canvas size
+TEMPLATE_WIDTH, TEMPLATE_HEIGHT = 1235, 1500
+CANVAS_WIDTH, CANVAS_HEIGHT = 2480, 3506
 
 # Gradio interface setup
 def gr_interface(image_data_list, font_size, vintage_polaroid):
@@ -13,11 +16,16 @@ def gr_interface(image_data_list, font_size, vintage_polaroid):
         image_data_list = [(img, txt) for img, txt in image_data_list if img is not None]
         num_images = len(image_data_list)
 
-        # Create an empty white A4 canvas
-        a4_canvas = Image.new('RGB', (A4_WIDTH, A4_HEIGHT), 'white')
+        # Create an empty white canvas with the dimensions you specified
+        canvas = Image.new('RGB', (CANVAS_WIDTH, CANVAS_HEIGHT), 'white')
 
-        # Coordinates for placing images in 4 corners
-        positions = [(0, 0), (A4_WIDTH//2, 0), (0, A4_HEIGHT//2), (A4_WIDTH//2, A4_HEIGHT//2)]
+        # Positions for placing images in 4 corners
+        positions = [
+            (0, 0),
+            (CANVAS_WIDTH - TEMPLATE_WIDTH, 0),
+            (0, CANVAS_HEIGHT - TEMPLATE_HEIGHT),
+            (CANVAS_WIDTH - TEMPLATE_WIDTH, CANVAS_HEIGHT - TEMPLATE_HEIGHT)
+        ]
 
         for i in range(num_images):
             input_img, text = image_data_list[i]
@@ -25,12 +33,39 @@ def gr_interface(image_data_list, font_size, vintage_polaroid):
                 input_img = Image.fromarray(input_img.astype('uint8'))
             overlayed_img = overlay_images(vintage_polaroid, input_img)
             if overlayed_img:
-                overlayed_img = overlayed_img.resize((A4_WIDTH//2, A4_HEIGHT//2), Image.LANCZOS)
+                overlayed_img = overlayed_img.resize((TEMPLATE_WIDTH, TEMPLATE_HEIGHT), Image.LANCZOS)
                 final_img = add_text_to_image(overlayed_img, text, int(font_size))
                 if final_img:
-                    a4_canvas.paste(final_img, positions[i])
+                    canvas.paste(final_img, positions[i])
 
-        return a4_canvas
+        # Get current date for folder naming
+        current_date = datetime.now().strftime('%Y-%m-%d')
+
+        # Create 'input' folder if it doesn't exist
+        if not os.path.exists('input'):
+            os.mkdir('input')
+
+        # Create a folder with the current date inside 'input'
+        input_date_folder = os.path.join('input', current_date)
+        if not os.path.exists(input_date_folder):
+            os.mkdir(input_date_folder)
+
+        # Save input images
+        for i, (input_img, text) in enumerate(image_data_list):
+            if isinstance(input_img, np.ndarray):
+                input_img = Image.fromarray(input_img.astype('uint8'), 'RGB')
+            input_img_path = os.path.join(input_date_folder, f"{text}.jpg")
+            input_img.save(input_img_path)
+
+        # Create 'output' folder if it doesn't exist
+        if not os.path.exists('output'):
+            os.mkdir('output')
+
+        # Save the output image
+        output_img_path = os.path.join('output', f"{current_date}.jpg")
+        canvas.save(output_img_path)
+
+        return canvas
     except Exception as e:
         print(f"Failed in Gradio interface: {e}")
         return None
